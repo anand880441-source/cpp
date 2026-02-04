033b22a2605fa4e1ddcc1d7cccb42ebc836fffa9
Anand Suthar <anand.suthar.cg@gmail.com>
2026-02-04 14:59:30 +0530

    Some basic Programming files Added

---
+++ index.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+// #include <cstdio>  //-> for printf() and scanf()
+using namespace std;
+
+int main() {
+    // string count1;
+
+    // cout << "Enter Your Name: ";
+    // cin >> count1;
+
+    // cout <<  "hii, " << count1 << "!" << endl;
+
+    for(int i = 1; i<=10; i++){
+        if(i%2 ==0){
+            cout <<i<<endl;
+        }
+    }
+
+    return 0;
+}

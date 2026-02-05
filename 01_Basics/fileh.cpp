91fbe4004bd4d564a06161d6478a5424eddd0663
Anand Suthar <anand.suthar.cg@gmail.com>
2026-02-05 15:22:04 +0530

    fileh.cpp added

---
+++ 01_Basics/fileh.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    ofstream file;
+
+    file.open("sample.txt",ios::app);
+
+    if(file.is_open()){
+        file << "First time using the file operation with the help of ofstream method." <<endl;
+        file.close();
+    }
+    return 0;
+}
